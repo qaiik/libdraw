5b2f56f585aa382c020b94cacdc60bbdd885491c
qaiik <82181231+qaiik@users.noreply.github.com>
2021-05-19 16:19:38 -0600

    Update draw.py

--- draw.py
+++ draw.py
@@ -5,16 +5,16 @@ def print_at(x, y, text):
      sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (x, y, text))
      sys.stdout.flush()
 
-if sys.argv[1] == '-h' or sys.argv[1] == '--help':
+if sys.argv[0] == '-h' or sys.argv[0] == '--help':
   print('Not yet')
 
-if sys.argv[1] == '-t' or sys.argv[1] == '-term':
-  if sys.argv[2] == 'at':
-    c = sys.argv[3].split(',')
-    text = ''.join(sys.argv[4:])
+if sys.argv[0] == '-t' or sys.argv[0] == '-term':
+  if sys.argv[1] == 'at':
+    c = sys.argv[2].split(',')
+    text = ''.join(sys.argv[3:])
     print_at(c[0], c[1], text)
 
-if sys.argv[1] == '-w' or sys.argv[1] == '-window':
+if sys.argv[0] == '-w' or sys.argv[0] == '-window':
   root = Tk()
 
   a = Label(root)
